649ed80b76701079704ccf76f42ed17230c03c2c
ytalo-breno <ytalobreno9@gmail.com>
2022-06-29 23:03:59 -0300

    Initial commit

---
+++ LexiconAutomaton.py
@@ -0,0 +1,14 @@
+
+# definindo letra e número
+letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+number = "0123456789"
+
+#definicao de estados
+
+
+#AUTOMATO (EU ACHO)
+dfa = {
+    0: {' ': 0,
+        number: 1,
+        }
+}

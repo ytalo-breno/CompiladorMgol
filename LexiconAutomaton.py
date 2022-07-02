6cb7a2eb373625c514d7c91ff8e5e0ebb7f5b4f3
ytalo-breno <ytalobreno9@gmail.com>
2022-07-02 16:20:58 -0300

    ajustes no teste do automato

--- LexiconAutomaton.py
+++ LexiconAutomaton.py
@@ -4,10 +4,9 @@ letter = tuple("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 number = tuple("0123456789")
 aritm = tuple("+-/*")
 
-#definicao de estados
 
 
-#AUTOMATO (EU ACHO)
+#tabela de transição de estados
 dfa = {
     0: {' ': 0,
         tuple('\"'+'\''): 1,
@@ -94,6 +93,89 @@ finalStates = {
 
 }
 
+tabela_simbolos = {
+    'inicio': {
+        'classe': 'inicio',
+        'lexema': 'inicio',
+        'tipo': 'inicio',
+    },
+    'varinicio': {
+        'classe': 'varinicio',
+        'lexema': 'varinicio',
+        'tipo': 'varinicio',
+    },
+    'varfim':{
+        'classe': 'varfim',
+        'lexema':'varfim',
+        'tipo': 'varfim',
+
+
+    },
+    'escreva':{
+        'classe':'escreva',
+        'lexema': 'escreva',
+        'tipo': 'escreva',
+    },
+    'leia':{
+        'classe':'leia',
+        'lexema':'leia',
+        'tipo':'tipo',
+    },
+    'se':{
+        'classe':'se',
+        'lexema':'se',
+        'tipo':'se',
+    },
+    'entao':{
+        'classe':'entao',
+        'lexema':'entao',
+        'tipo':'entao',
+    },
+    'fimse':{
+        'classe':'fimse',
+        'lexema':'fimse',
+        'tipo':'fimse',
+    },
+    'repita':{
+        'classe':'repita',
+        'lexema':'repita',
+        'tipo':'repita',
+    },
+    'fimrepita':{
+        'classe':'fimrepita',
+        'lexema':'fimrepita',
+        'tipo': 'fimrepita',
+    },
+    'fim':{
+        'classe':'fim',
+        'lexema':'fim',
+        'tipo':'fim',
+    },
+    'inteiro':{
+        'classe':'inteiro',
+        'lexema':'inteiro',
+        'tipo':'inteiro',
+    },
+    'literal':{
+        'classe':'literal',
+        'lexema':'literal',
+        'tipo':'literal',
+    },
+    'real':{
+        'classe':'real',
+        'lexema':'real',
+        'tipo':'real',
+    }
+}
+
+def inserir_tabela_simbolos(tabela_simbolos,classe,token,tipo):
+    tabela_simbolos[token] ={
+        'classe': classe,
+        'lexema': token,
+        'tipo': tipo
+    }
+#inserir_tabela_simbolos(tabela_simbolos,)
+
 
 def teste(trans, init, aceitacao ,string):
     #breakpoint()
@@ -105,6 +187,5 @@ def teste(trans, init, aceitacao ,string):
         return aceitacao[estado]
 
 
-teste(dfa,0 , finalStates,)
-#print(dfa[0])
-
+teste(dfa,0 , finalStates,'1.1')
+print (tabela_simbolos['inicio']['classe'])

12fc29b8f4d589b45a3a6a9dcd925d9d11ba5f3c
ytalo-breno <ytalobreno9@gmail.com>
2022-08-07 10:41:19 -0300

    ajustes na função scanner

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import string
+import pandas as pd
+
 
 from AutomatoLexico import *
 from TabelaSimbolos import *
@@ -7,15 +8,28 @@ from TabelaSimbolos import *
 def main():
 
     arq = open('./files/teste.mgol', "r")
+    action = pd.read_csv('./files/actions.csv',index_col=0)
+    goto = pd.read_csv('./files/goto.csv',index_col=0)
+
     arq_lido = arq.read()
     marcador = 0
     token = {'classe': ''}
+
+    s = 0
+    pilha = []
+
     while token.get('classe') != 'EOF':
+        #estrutura de chamada do léxico
         token, marcador = scanner(afd, estadosFinais, tabela_simbolos, arq_lido,marcador)
         arq.seek(marcador)
         arq_lido = arq.read()
-        print(token)
 
+        #sintatico
+        #if (action[])
+
+
+    print(action)
+    print(goto)
 if __name__ == '__main__':
   main()
 
